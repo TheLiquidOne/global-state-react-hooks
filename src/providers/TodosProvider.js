9b15deaa4a88a3620e1e183bd34c7455a415a120
Nicolas Tang <nicolas.tang@epita.fr>
2019-05-27 15:21:45 +0200

    Remove unused files

--- src/providers/TodosProvider.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const TodoContext = React.createContext()
-
-export const TodosProvider = ({reducer, initialState, children}) => (
-    <TodoContext.Provider value={React.useReducer(reducer, initialState)}>
-        {children}
-    </TodoContext.Provider>
-)
